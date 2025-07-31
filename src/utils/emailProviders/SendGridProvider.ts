e14789ccf3419c0877d37d402bcd262f7b6046ef
Daria Kravchuk <daria1kravchuk@gmail.com>
2025-07-31 19:08:43 +0300

    e2e tests ci pipeline setup

--- src/utils/emailProviders/SendGridProvider.ts
+++ src/utils/emailProviders/SendGridProvider.ts
@@ -14,13 +14,10 @@ export class SendGridProvider implements IEmailProvider {
         html: string;
     }): Promise<any> {
         try {
-            const response = await sgMail.send({
-                ...msg, mailSettings: {
-                    sandboxMode: {
-                        enable: process.env.NODE_ENV === 'test',
-                    }
-                }
-            });
+            if (process.env.NODE_ENV === 'test')
+                return;
+
+            const response = await sgMail.send(msg);
             console.log(`Email sent to ${msg.to}`, response);
             return response;
         } catch (error) {
